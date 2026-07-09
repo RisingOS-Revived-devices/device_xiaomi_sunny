4345f5ec9e059956c3a6da781c580334d7acad95
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-09 05:23:41 +0000

    sunny: fastbootd is now enabled by default
    Change-Id: Ifcfbc2d3462d676d1e8cff03caaed630182e04d2
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -184,10 +184,6 @@ PRODUCT_PACKAGES += \
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.lineage
