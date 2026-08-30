PixelOS-Devices/android_device_xiaomi_sunny

sunny: Don't use BSG framework for gpt-utils
Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

device.mk
@@ -97,6 +97,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080

RisingOS-Revived-devices/device_xiaomi_sunny

device.mk
@@ -97,6 +97,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+$(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080